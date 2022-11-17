7b5700a20690afa640f907165eed2edfaec7a7b0
Mayank Thakur <54767390+Mynkthkr@users.noreply.github.com>
2022-11-17 23:45:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('hello , This is Version 1 ! ');
+  res.send('Hello , This is Version 1 ! ');
 });
 
 app.listen(PORT, HOST);
